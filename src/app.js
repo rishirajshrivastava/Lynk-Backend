34e8ef13c784b5fb78e4fcc7ec7ef815461593f2
Rishi <rishirajshrivastava44@gmail.com>
2025-08-12 00:59:49 +0530

    adding signup API for user to add user to DB(hardcode3d for now will update soon)

--- src/app.js
+++ src/app.js
@@ -1,16 +1,33 @@
 const express = require('express');
-
+const connectDB = require('./config/database');
+const User = require('./models/user');
 const app = express();
-app.listen(3000, () => {
 
-    app.use("/test",(req,res) =>{
-        res.send('test!');
-    })
-    app.use("/hello",(req,res) =>{
-        res.send('Hello, World!');
-    })
-    app.use("/",(req,res) =>{
-        res.send('gtyhsrth');
-    })
+app.post("/signup",async (req,res)=>{
+    const userObj = {
+        firstName: "viart",
+        lastName: "kohli",
+        email: "kohliavi@gmail.com",
+        password: "kohli123",
+        age: 25,
+        gender: "male"
+    }
+    //creating a new instance of User model
+    const user = new User(userObj);
+    try{
+        await user.save();
+        res.send("user added successfully");
+    } catch (err) {
+        res.status(500).send("Error saving user"+ err.message);
+    }
+    
+})
+
+connectDB().then(()=>{
+    console.log('Connected to database successfully');
+    app.listen(3000, () => {    
     console.log('Server is running on port 3000');
 });
+}).catch(err=>{
+    console.log('Error connecting to database:', err);
+})
---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    await mongoose.connect(
+        "mongodb+srv://shrivastavarishiraj9:WmiQYY38sUPutTUV@cluster0.ybp4jpc.mongodb.net/devTinder"
+    ); 
+}
+
+module.exports = connectDB;
