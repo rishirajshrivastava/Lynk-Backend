98ccbc3e570175c6b8f9167f193d8593a01afa00
Rishi <rishirajshrivastava44@gmail.com>
2025-09-02 23:11:33 +0530

    updated DB password

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     await mongoose.connect(
-        "mongodb+srv://shrivastavarishiraj9:WmiQYY38sUPutTUV@cluster0.ybp4jpc.mongodb.net/lynk"
+        "mongodb+srv://shrivastavarishiraj9:Rishiraj%401608@cluster0.ybp4jpc.mongodb.net/lynk"
     ); 
 }
 
