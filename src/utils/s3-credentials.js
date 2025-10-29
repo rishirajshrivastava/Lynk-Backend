61ee5364a6ebc054de8b6fea428d7a9927d5de20
Rishi <rishirajshrivastava44@gmail.com>
2025-10-29 17:58:41 +0530

    added logs

--- src/utils/s3-credentials.js
+++ src/utils/s3-credentials.js
@@ -5,6 +5,23 @@ console.log("AWS_SECRET_KEY", process.env.AWS_SECRET_KEY);
 console.log("AWS_REGION", process.env.AWS_REGION);
 console.log("AWS_S3_BUCKET", process.env.AWS_S3_BUCKET);
 
+// Validate required environment variables
+if (!process.env.AWS_REGION) {
+    throw new Error('AWS_REGION environment variable is not set. Please configure it in your .env file.');
+}
+
+if (!process.env.AWS_ACCESS_KEY) {
+    throw new Error('AWS_ACCESS_KEY environment variable is not set. Please configure it in your .env file.');
+}
+
+if (!process.env.AWS_SECRET_KEY) {
+    throw new Error('AWS_SECRET_KEY environment variable is not set. Please configure it in your .env file.');
+}
+
+if (!process.env.AWS_S3_BUCKET) {
+    throw new Error('AWS_S3_BUCKET environment variable is not set. Please configure it in your .env file.');
+}
+
 exports.s3Client = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
